540473515f5840316384897e6cb3eb0b02ddc7c9
Pavel Lvou <pavel.lvou@hiqo-solutions.com>
2025-06-25 20:51:43 +0300

    Task complete

---
+++ Net21/FirstConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿var theNumber = 42;
+
+int guess;
+do
+{
+    Console.WriteLine("Guess the number");
+    var guessString = Console.ReadLine();
+    if (!int.TryParse(guessString, out guess))
+    {
+        Console.WriteLine("It's not a number");
+        continue;
+    }
+
+    if (guess > theNumber)
+    {
+        Console.WriteLine("less");
+    }
+    else if (guess < theNumber)
+    {
+        Console.WriteLine("more");
+    }
+} while (guess != theNumber);
+
+Console.WriteLine("You are win");
