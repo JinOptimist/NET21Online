ab0f283df5e79e414613e3c51a1d6e050cc546e5
Daniel Sinke <danik3559@gmail.com>
2025-06-28 01:02:53 +0300

    add user name, step count  to win,  alert of repeat step, min/max for guess ana update

--- Net21/FirstConsole/Program.cs
+++ Net21/FirstConsole/Program.cs
@@ -1,38 +1,86 @@
-﻿var theNumber = 42;
+﻿var theNumber = 42; 
 var name = "user";
+var attempts = 0;
 int guess;
+var maxCount = 20; // число попыток
+int? previousNumber = null;
+int? currentAttempt = null;
+var minInterval = 0;
+var maxInterval = 100;
 
-do 
+    do // ввод имени пользователя + проверка на пустое поле 
     {
-    Console.WriteLine("Enter your name:");
-    name = Console.ReadLine();
-    if (string.IsNullOrEmpty(name))
-    {
-        Console.WriteLine("The field name is empty. Please Enter your name");
-        continue;
-    }
-    }  while (string.IsNullOrEmpty(name));
+        Console.WriteLine("Enter your name:");
+        name = Console.ReadLine();
+    
+            if (string.IsNullOrEmpty(name))
+            {
+                Console.WriteLine("The field name is empty. Please Enter your name");
+                continue;
+            }
+}  while (string.IsNullOrEmpty(name));
 
     do
     {
-        Console.WriteLine($"Guess the number {name}");
+        currentAttempt = maxCount - attempts;
+        Console.WriteLine($"Guess the number {name}. You have only {currentAttempt} attempts. The interval between {minInterval} and  {maxInterval} ");
         var guessString = Console.ReadLine();
-        if (!int.TryParse(guessString, out guess))
+    
+    if (!int.TryParse(guessString, out guess))
         {
             Console.WriteLine("It's not a number");
             continue;
         }
-
-        if (guess > theNumber)
+       
+    
+    if (guess==previousNumber) //сравнение с предыдущим числом
         {
-            Console.WriteLine("less");
+        Console.WriteLine("You've already wrote it");
+        continue;
+        }
+        previousNumber = guess;
+
+    if (guess > maxInterval) //мин/макс интервал и изменения диапазона
+        {
+        Console.WriteLine("Your number is more than max Limit. Your move is not counted.");
+        attempts--;
+        }
+        else if (guess < minInterval)
+        {
+        Console.WriteLine("Your number is less than min Limit. Your move is not counted.");
+        attempts--;
+        }
+        else if (guess> theNumber)
+        {
+        maxInterval = guess;
         }
         else if (guess < theNumber)
-        {
-            Console.WriteLine("more");
+        { 
+        minInterval = guess; 
         }
-    } while (guess != theNumber);
 
-Console.WriteLine($"You are win {name}"); 
 
+    if (guess > theNumber) // логика мор/лесс
+        {
+            Console.WriteLine("less");
+           
+        }
+            else if (guess < theNumber)
+        {
+            Console.WriteLine("more");  
+        }
+
+
+    attempts++; 
+        
+    if (attempts >= maxCount) // проверка на лимит ходов
+    {
+        Console.WriteLine($"Loser. You've reached {maxCount} attempts ");
+        return;
+        }
+
+} while (guess != theNumber);
+
+Console.WriteLine($"You are win {name}");
+Console.WriteLine($"You made {attempts} attempts");
 
