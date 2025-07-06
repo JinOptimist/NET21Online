e651fc4b6f4f772f8d3cc06028953dbac4418593
Xastur <Xastur@DESKTOP-LKTV7TE>
2025-07-06 21:26:06 +0300

    Hometask part 1

--- Net21/BullsAndCows/BullAndCowHumanVsHuman.cs
+++ Net21/BullsAndCows/BullAndCowHumanVsHuman.cs
@@ -1,4 +1,6 @@
-﻿namespace BullsAndCows
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace BullsAndCows
 {
     /// <summary>
     /// Human set secret. Human try to get answer
@@ -6,18 +8,64 @@
     internal class BullAndCowHumanVsHuman : BullAndCowBase
     {
         protected override string GenerateSecret()
+
         {
-            throw new NotImplementedException();
+            static string GetUniqueFourDigitNumberAsString()
+            {
+                while (true)
+                {
+                    Console.Write("Please enter a four digit number: ");
+                    string input = Console.ReadLine();
+
+                    if (input.Length != 4 || !int.TryParse(input, out int number) || number < 1000 || number > 9999)
+                    {
+                        Console.WriteLine("Error: number must be four digits ");
+                        continue;
+                    }
+
+                    int digit1 = number / 1000;
+                    int digit2 = (number / 100) % 10;
+                    int digit3 = (number / 10) % 10;
+                    int digit4 = number % 10;
+
+                    if (digit1 != digit2 && digit1 != digit3 && digit1 != digit4 &&
+                        digit2 != digit3 && digit2 != digit4 &&
+                        digit3 != digit4)
+                    {
+                        return input;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: numbers must be different ");
+                    }
+                }
+            }
+
+
+            string answer = GetUniqueFourDigitNumberAsString();
+
+
+            Console.Clear();
+
+            return answer;
         }
 
         protected override string GetGuess()
         {
-            throw new NotImplementedException();
+            Console.WriteLine("Guess my number. Enter 4 diff number");
+            var answer = Console.ReadLine();
+
+            while (answer == null || answer.Length != 4 || !answer.All(char.IsDigit))
+            {
+                Console.WriteLine("Please enter exactly 4 digits. Try again:");
+                answer = Console.ReadLine();
+            }
+            return answer;
         }
 
         protected override void GetResponse((int bull, int cow) bullAndCow)
         {
-            throw new NotImplementedException();
+            Console.WriteLine($"bull: {bullAndCow.bull} cow: {bullAndCow.cow}");
         }
     }
 }
--- Net21/BullsAndCows/Program.cs
+++ Net21/BullsAndCows/Program.cs
@@ -1,4 +1,21 @@
 ﻿using BullsAndCows;
 
-var game = new BullAndCowBotVsHuman();
-game.Play();
+
+
+Console.WriteLine("Enter 1 if you want play with bot. " +
+                "\nEtner 2 if you have a friend");
+var gameMode = Console.ReadLine();
+switch (gameMode)
+{
+    case ("1"):
+       var game = new BullAndCowBotVsHuman();
+        game.Play();
+        break;
+    case ("2"):
+        var game2 = new BullAndCowHumanVsHuman();
+        game2.Play();
+        break;
+    default:
+        Console.WriteLine("wrong number");
+        return;
+}
