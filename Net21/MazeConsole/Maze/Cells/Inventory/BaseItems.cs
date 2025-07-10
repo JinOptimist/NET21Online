69eb9b1a3ac3d165a9f230d6aec3bd35b6e7c8a3
Ростислав Мошко <koshakcrosts@gmail.com>
2025-07-10 15:29:59 +0300

    Add new type cell [~] - sea; and create new Class for items in inventory with item [^] - boat

---
+++ Net21/MazeConsole/Maze/Cells/Inventory/BaseItems.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MazeConsole.Maze.Cells.Inventory
+{
+    /// <summary>
+    /// Class for items that can be in inventory
+    /// </summary>
+    public abstract class BaseItems : BaseCell
+    {
+        protected BaseItems(int x, int y, MazeMap mazeMap) : base(x, y, mazeMap)
+        {
+
+        }
+
+        public bool CanGet(Hero hero) => hero.SizeInventory >= hero.Inventory.Count + 1;
+    }
+}
---
+++ Net21/MazeConsole/Maze/Cells/Inventory/Sea.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MazeConsole.Maze.Cells.Inventory
+{
+    public class Sea : BaseCell
+    {
+        public Sea(int x, int y, MazeMap mazeMap) : base(x, y, mazeMap)
+        {
+
+        }
+
+        public override string Symbol => "~";
+
+        public override bool TryStep(Hero hero)
+        {
+            return hero.Inventory.Any(item => item is Boat);
+        }
+    }
+}
