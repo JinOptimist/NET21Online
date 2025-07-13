7e5781567ff23e8743c7d1871d85c61b2b25d4f5
Alex-Orel34 <Alex.orlovich@gmail.com>
2025-07-13 16:13:35 +0300

    Delete parametr BuidTrap

--- Net21/MazeConsole/Builder/MazeBuilder.cs
+++ Net21/MazeConsole/Builder/MazeBuilder.cs
@@ -75,7 +75,7 @@ namespace MazeConsole.Builder
         {
             var validCells = _currentSurface.CellsSurface
                 .OfType<Ground>()
-                .Where(cell => !(cell is Hero) && !(cell is Wall)) 
+                .Where(cell => !(cell is Wall)) 
                 .ToList();
 
             var random = new Random();
