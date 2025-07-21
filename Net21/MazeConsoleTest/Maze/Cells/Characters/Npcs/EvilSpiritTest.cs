35ee5e2ddf4c9cbbe788797501f600a61c4a57db
Isyuzev <Isyuzev@yandex.by>
2025-07-21 14:39:00 +0300

    First attempt

---
+++ Net21/MazeConsoleTest/Maze/Cells/Characters/Npcs/EvilSpiritTest.cs
@@ -0,0 +1,103 @@
+﻿using MazeConsole.Maze.Cells.Surface;
+using MazeConsole.Maze.Cells.Сharacters;
+using MazeConsole.Maze.Cells.Сharacters.Npcs;
+using MazeConsole.Maze;
+using Moq;
+using NUnit.Framework;
+using MazeConsole.Maze.Cells;
+using MazeConsole.Maze.Cells.Characters.Npcs;
+
+namespace MazeConsole.Tests.Maze.Cells.Сharacters.Npcs
+{
+    [TestFixture]
+    public class EvilSpiritTests
+    {
+        private Mock<IMazeMap> _mazeMapMock;
+        private Mock<IBaseCharacter> _baseCharacterMock;
+        private EvilSpirit _evilSpirit;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mazeMapMock = new Mock<IMazeMap>();//stub
+            _baseCharacterMock = new Mock<IBaseCharacter>();//stub
+            _evilSpirit = new EvilSpirit(0, 0, _mazeMapMock.Object);//real
+        }
+
+        [Test]
+        public void CellToFirstMoveHero()
+        {
+            var hero = new Hero(1, 0, _mazeMapMock.Object);
+            var ground = new Ground(0, 1, _mazeMapMock.Object);
+
+            _mazeMapMock.Setup(m => m.GetNearCell(It.IsAny<BaseCell>()))
+                .Returns(new BaseCell[] { hero, ground });
+
+            // Act
+            var result = _evilSpirit.CellToMove();
+
+            // Assert
+            Assert.That(_evilSpirit.CellToMove(), Is.SameAs(hero));
+        }
+
+        [Test]
+        public void CellToSecondMoveGround()
+        {
+            // Arrange
+            var ground = new Ground(0, 1, _mazeMapMock.Object);
+            var wall = new Wall(1, 0, _mazeMapMock.Object);
+
+            _mazeMapMock.Setup(m => m.GetNearCell(It.IsAny<BaseCell>()))
+                .Returns(new BaseCell[] { ground, wall });
+
+            // Act
+            var result = _evilSpirit.CellToMove();
+
+            // Assert
+            Assert.That(_evilSpirit.CellToMove(), Is.SameAs(ground));
+        }
+
+        [Test]
+        public void CellToMoveNoGroundNull()
+        {
+            // Arrange
+            var wall = new Wall(1, 0, _mazeMapMock.Object);
+
+            _mazeMapMock.Setup(m => m.GetNearCell(It.IsAny<BaseCell>()))
+                .Returns(new BaseCell[] { wall });
+
+            // Act
+            var result = _evilSpirit.CellToMove();
+
+            // Assert
+            Assert.That(_evilSpirit.CellToMove(), Is.Null);
+        }
+        [Test]
+        public void TryStepEvilSpirit()
+        {
+            // Arrange
+            var otherEvilSpirit = new EvilSpirit(1, 0, _mazeMapMock.Object);
+
+            // Act
+            var result = _evilSpirit.TryStep(otherEvilSpirit);
+
+            // Assert
+            Assert.That(result,Is.True);
+        }
+
+        [Test]
+        public void TryStepDecreasesHp()
+        {
+            // Arrange
+            var hero = new Hero(1, 0, _mazeMapMock.Object) { Hp = 1};
+            var initialHp = hero.Hp;
+
+            // Act
+            var result = _evilSpirit.TryStep(hero);
+
+            // Assert
+            Assert.That(result,Is.False);
+            Assert.That(hero.Hp, Is.EqualTo(initialHp - 1));
+        }
+    }
+}
---
+++ Net21/MazeConsoleTest/Maze/Cells/ShieldTest.cs
@@ -0,0 +1,81 @@
+﻿using MazeConsole.Maze.Cells.Surface;
+using MazeConsole.Maze.Cells.Сharacters;
+using MazeConsole.Maze;
+using Moq;
+using NUnit.Framework;
+using MazeConsole.Maze.Cells;
+
+namespace MazeConsoleTest.Maze.Cells
+{
+    public class ShieldTest
+    {
+        private Mock<IMazeMap> _mazeMapMock;
+        private Mock<IBaseCharacter> _baseCharacterMock;
+        private Shield _shield;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mazeMapMock = new Mock<IMazeMap>();// Stub
+            _baseCharacterMock = new Mock<IBaseCharacter>();// Stub
+            _shield = new Shield(28, 23, _mazeMapMock.Object); // Real
+        }
+
+        [Test]
+        [TestCase(1, 3)]
+        [TestCase(10, 12)]
+        public void TryStep_CheckThatHpIsIncrease(int initialHp, int resultHp)
+        {
+            // Arrange / preparation
+            _baseCharacterMock.SetupAllProperties();
+            var hero = _baseCharacterMock.Object;
+            hero.Hp = initialHp;
+
+            //  Act
+            _shield.TryStep(hero);
+
+            // Assert
+            Assert.That(hero.Hp == resultHp, "Shield add 2 hp it's a problem");
+        }
+
+        [Test]
+        public void TryStep_ReturnTrue()
+        {
+            // Arrange / preparation
+
+            //  Act
+            var result = _shield.TryStep(_baseCharacterMock.Object);
+
+            // Assert
+            Assert.That(result == true, "I must have possibility step on shield");
+        }
+
+        [Test]
+        [TestCase(-1)]
+        [TestCase(-42)]
+        public void TryStep_ThrowExceptionWithNegativaInitialHp(int initialHp)
+        {
+            // Arrange / preparation
+            var mazeMapMock = new Mock<IMazeMap>();
+            var mazeMap = mazeMapMock.Object; // Stub
+
+            // Act
+            // Assert
+            Assert.Throws<ArgumentException>(
+                () => new Coin(28, 23, mazeMap, initialHp),
+                "We expect exception with negative Hp");
+        }
+
+        [Test]
+        public void TryStep_CellWasReplaced()
+        {
+            // Arrange / preparation
+
+            // Act
+            _shield.TryStep(_baseCharacterMock.Object);
+
+            // Assert
+            _mazeMapMock.Verify(maze => maze.ReplaceCell(It.IsAny<Ground>()), Times.Once);
+        }
+    }
+}
