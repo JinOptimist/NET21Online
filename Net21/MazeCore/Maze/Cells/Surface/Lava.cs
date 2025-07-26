487f563aac840127b5185b717fc256ded551e26b
Andrei <146109490+AndreyDud@users.noreply.github.com>
2025-07-26 19:06:06 +0300

    Lava - [№]

---
+++ Net21/MazeCore/Maze/Cells/Surface/Lava.cs
@@ -0,0 +1,20 @@
+using MazeCore.Maze.Cells.Characters;
+
+namespace MazeCore.Maze.Cells.Surface
+{
+    public class Lava : BaseCell
+    {
+        public Lava(int x, int y, IMazeMap mazeMap) : base(x, y, mazeMap)
+        {
+        }
+
+        public override string Symbol => "№";
+
+        public override bool TryStep(IBaseCharacter character)
+        {
+            character.Hp = 0;
+            character.Money = 0;
+            return false;
+        }      
+    }
+}
