3fd18eaa8bf0ea5824e467651c18225788d9f0a3
Pavel Lvou <pavel.lvou@hiqo-solutions.com>
2025-08-04 21:16:11 +0300

    Create WebPortal

---
+++ Net21/WebPortal/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using WebPortal.Models;
+
+namespace WebPortal.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Smile(int age, string name, bool isRed)
+        {
+            var yearOfBirthday = DateTime.Now.Year - age;
+
+            var viewModel = new SmileViewModel();
+            viewModel.Name = name + 
+                (isRed ? " soulless" : "");
+            viewModel.YearOfBirthday = yearOfBirthday;
+            return View(viewModel);
+        }
+    }
+}
