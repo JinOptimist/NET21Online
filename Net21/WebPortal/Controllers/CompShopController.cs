5e23e4112d6ab27b4934e6e548ee641a9e54f4dd
Ростислав Мошко <koshakcrosts@gmail.com>
2025-08-05 21:52:14 +0300

    Add my html/css to C# project

---
+++ Net21/WebPortal/Controllers/CompShopController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebPortal.Models;
+
+namespace WebPortal.Controllers
+{
+    public class CompShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            var listDevices = new List<DeviceModel>();
+
+            //Получение всех устройс, где Popular = true из бд
+
+            if (!listDevices.Any())
+            {
+                listDevices.AddRange(new[]
+                {
+                        new DeviceModel
+                        {
+                            Name = "RTX 4060 Ti/Ryzen 5 5600",
+                            TypeDevice = Typedevice.Computer,
+                            DeviceCategory = DeviceCategory.Gaming,
+                            Price = 3200,
+                            Image = @"/images/Moshko/index/comp1.jpg"
+                        },
+                        new DeviceModel
+                        {
+                            Name = "RTX 4060 Ti/Ryzen 5 5600",
+                            TypeDevice = Typedevice.Computer,
+                            DeviceCategory = DeviceCategory.Gaming,
+                            Price = 3200,
+                            Image = @"/images/Moshko/index/comp1.jpg"
+                        },
+                        new DeviceModel
+                        {
+                            Name = "RTX 4060 Ti/Ryzen 5 5600",
+                            TypeDevice = Typedevice.Computer,
+                            DeviceCategory = DeviceCategory.Gaming,
+                            Price = 3500,
+                            Image = @"/images/Moshko/index/comp1.jpg"
+                        },
+                        new DeviceModel
+                        {
+                            Name = "RTX 4060 Ti/Ryzen 5 5600",
+                            TypeDevice = Typedevice.Computer,
+                            DeviceCategory = DeviceCategory.Gaming,
+                            Price = 3200,
+                            Image = @"/images/Moshko/index/comp1.jpg"
+                        },
+                        new DeviceModel
+                        {
+                            Name = "RTX 4060 Ti/Ryzen 5 5600",
+                            TypeDevice = Typedevice.Laptop,
+                            DeviceCategory = DeviceCategory.Office,
+                            Price = 3500,
+                            Image = @"/images/Moshko/index/comp1.jpg"
+                        }
+                    });
+            }
+
+            return View(listDevices);
+        }
+    }
+}
