b2ab4dd7b019c6ee28f345a5ae047c46e665f342
Artiom Zelianok <artemzelenok60@gmail.com>
2025-08-06 19:44:08 +0300

    Add minifix

--- Net21/WebPortal/Controllers/CoffeShopController.cs
+++ Net21/WebPortal/Controllers/CoffeShopController.cs
@@ -11,18 +11,18 @@ namespace WebPortal.Controllers
             {
                 CoffeeProducts = new List<string>
                 {
-                    "images/coffeshop/p1.png",
-                    "images/coffeshop/p2.png",
-                    "images/coffeshop/p3.png",
-                    "images/coffeshop/p4.png",
-                    "images/coffeshop/p5.png",
-                    "images/coffeshop/p6.png"
+                    "/images/coffeshop/p1.png",
+                    "/images/coffeshop/p3.png",
+                    "/images/coffeshop/p2.png",
+                    "/images/coffeshop/p4.png",
+                    "/images/coffeshop/p5.png",
+                    "/images/coffeshop/p6.png"
                 },
                 UserComments = new List<string>
                 {
-                    "images/coffeshop/rev1.jpg",
-                    "images/coffeshop/rev2.jpg",
-                    "images/coffeshop/rev3.jpg"
+                    "/images/coffeshop/rev1.jpg",
+                    "/images/coffeshop/rev2.jpg",
+                    "/images/coffeshop/rev3.jpg"
                 }
             };
 
