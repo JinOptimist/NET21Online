b06d97882c1885cbb7c21a4cf8223c5d87de0378
Ростислав Мошко <koshakcrosts@gmail.com>
2025-08-06 21:19:52 +0300

    Set Name

--- Net21/WebPortal/Models/CompShop/DeviceViewModel.cs
+++ Net21/WebPortal/Models/CompShop/DeviceViewModel.cs
@@ -8,14 +8,19 @@
 
         public string? Description { get; set; }
 
-        public TypeDevice? TypeDevice { get; set; }
+        public int? TypeDeviceId { get; set; }  
 
-        public Category? Category { get; set; }
+
+        public int? CategoryId { get; set; }
 
         public double? Price { get; set; }
 
         public string? Image { get; set; }
 
         public bool IsPopular { get; init; } //Only for Admins, default user can't set
+
+
+        public TypeDevice? TypeDevice { get; set; }
+        public Category? Category { get; set; }
     }
 }
