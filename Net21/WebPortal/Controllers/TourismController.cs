0fcba85945ec2b85408e1a8b36e74fc99fd34e1d
Daniel Sinke <danik3559@gmail.com>
2025-08-10 17:49:55 +0300

    add creation page

--- Net21/WebPortal/Controllers/TourismController.cs
+++ Net21/WebPortal/Controllers/TourismController.cs
@@ -1,12 +1,49 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebPortal.Models;
+using WebPortal.Models.Tourism;
 
 namespace WebPortal.Controllers
 {
     public class TourismController : Controller
     {
+        private static List<TourismViewModel> Tourism = new List<TourismViewModel>();
         public IActionResult Index()
         {
+            if (!Tourism.Any())
+            {
+                var titleNames = new List<TourismViewModel>
+            {
+                new TourismViewModel
+                {
+                   Title = "Trip to Grodno",
+                   URL = "/images/tourism/grodno.jpg",
+                   Days = 3
+                },
+                new TourismViewModel
+                {
+                   Title = "Trip to Vitebs",
+                   URL = "/images/tourism/vitebsk.jpg",
+                   Days = 5
+                }
+            };
+                Tourism.AddRange(titleNames);
+            }
+
+            return View(Tourism);
+        }
+
+        [HttpGet]
+        public IActionResult AddContent()
+        {
             return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult AddContent(TourismViewModel viewModel)
+        {
+            Tourism.Add(viewModel);
+            return RedirectToAction("Index");
         }
     }
 }
