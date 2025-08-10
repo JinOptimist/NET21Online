784c2085991327644d92184d9e44e8bb4ae79911
Artiom Zelianok <artemzelenok60@gmail.com>
2025-08-10 15:22:36 +0300

    Add Coffe and Add Users Comments

--- Net21/WebPortal/Controllers/CoffeShopController.cs
+++ Net21/WebPortal/Controllers/CoffeShopController.cs
@@ -1,34 +1,87 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using WebPortal.Models;
+using WebPortal.Models.CoffeShop;
 
 namespace WebPortal.Controllers
 {
     public class CoffeShopController : Controller
     {
+        //Delete it in next day
+        private static List<CoffeeProduct> CoffeProd = new List<CoffeeProduct>();
+        private static List<UserComment> UserCom = new List<UserComment>();
+
+
         public IActionResult Index()
         {
+            if (!CoffeProd.Any())
+            {
+                string[] names = { "Espresso", "Latte", "Arabic coffee", "Cappuccino", "Mocha", "Flat White" };
+                int[] cell = { 35, 44, 55, 25, 60, 15 };
+                for (int i = 0; i < names.Length; i++)
+                {
+                    var coffee = new CoffeeProduct
+                    {
+                        Img = $"/images/coffeshop/p{i + 1}.png",
+                        Name = names[i],
+                        Cell = cell[i]
+                    };
+
+                    CoffeProd.Add(coffee);
+                }
+            }
+
+            if (!UserCom.Any())
+            {
+                string[] userName = { "Name1", "Name2", "Name3" };
+                string[] description = { "Nice Coffe", "Top Coffe", "I love tropical coffe" };
+                for (int i = 0; i < userName.Length; i++)
+                {
+                    var userProfile = new UserComment
+                    {
+                        ImgUser = $"/images/coffeshop/rev{i + 1}.jpg",
+                        NameUser = userName[i],
+                        Description = description[i]
+                    };
+
+                    UserCom.Add(userProfile);
+                }
+
+            }
             var model = new CoffeeShopViewModel
             {
-                CoffeeProducts = new List<string>
-                {
-                    "/images/coffeshop/p1.png",
-                    "/images/coffeshop/p3.png",
-                    "/images/coffeshop/p2.png",
-                    "/images/coffeshop/p4.png",
-                    "/images/coffeshop/p5.png",
-                    "/images/coffeshop/p6.png"
-                },
-                UserComments = new List<string>
-                {
-                    "/images/coffeshop/rev1.jpg",
-                    "/images/coffeshop/rev2.jpg",
-                    "/images/coffeshop/rev3.jpg"
-                }
+                CoffeeProducts = CoffeProd,
+                UserComments = UserCom
             };
 
-            return View(model);  
+            return View(model);
         }
 
+        [HttpGet]
+        public IActionResult AddCoffe()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddCoffe(CoffeeProduct viewcoffe)
+        {
+            CoffeProd.Add(viewcoffe);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult AddComents()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddComents(UserComment userComments)
+        {
+            UserCom.Add(userComments);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult LoginPageTest()
+        {
+            return View();
+        }
     }
 }
 
--- Net21/WebPortal/Models/CoffeeShopViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace WebPortal.Models
-{
-    public class CoffeeShopViewModel
-    {
-        public List<string> CoffeeProducts { get; set; }
-        public List<string> UserComments { get; set; }
-    }
-}
