cbdf12a52e4c343e079f9ced3a3fc5395bf8be85
Dmitry Varchak <dmitvar@gmail.com>
2025-08-11 18:50:01 +0300

    add a page with a form for adding motorcycles

--- Net21/WebPortal/Controllers/MotorcyclesController.cs
+++ Net21/WebPortal/Controllers/MotorcyclesController.cs
@@ -1,12 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using WebPortal.Models;
+using WebPortal.Models.Motorcycles;
 
 namespace WebPortal.Controllers
 {
     public class MotorcyclesController : Controller
     {
+        // DO NOT REPEAT IT
+        // REMOVE THIS CODE AFTER WE ADD DataBase
+        // It is necessary to redo it. Use only the database!!!!!!!!!!!!!!
+        private static List<MotorcyclesViewModel> Motorcycles = new List<MotorcyclesViewModel>();
+
         public IActionResult Index()
         {
+            if (!Motorcycles.Any()) 
+            {
+                for (int i = 0; i < 4; i++) 
+                {
+                    var model = new MotorcyclesViewModel();
+                    model.Src = $"/images/motorcycles/moto{i}.jpg";
+                    model.Name = $"BIKE{i + 1}";
+                    model.Description = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam quo autem, perferendis voluptatum enim sunt non totam ducimus dignissimos animi cum velit ipsa veniam. Quasi porro fugiat commodi distinctio consequatur!";
+                    Motorcycles.Add(model);
+                }
+            }
+
+            return View(Motorcycles);
+        }
+
+        [HttpGet]
+        public IActionResult AddBike() 
+        {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddBike(MotorcyclesViewModel model) 
+        {
+            Motorcycles.Add(model);
+            return RedirectToAction("Index");
         }
     }
 }
