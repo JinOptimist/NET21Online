05bdb3114a9d483cabbc3032796067984743d3c2
Pavel Lvou <pavel.lvou@hiqo-solutions.com>
2025-08-11 21:23:08 +0300

    Add database

---
+++ Net21/WebPortal/DbStuff/Models/Anime.cs
@@ -0,0 +1,8 @@
+﻿namespace WebPortal.DbStuff.Models
+{
+    public class Anime : BaseModel
+    {
+        public string Name { get; set; }
+        public DateTime ReleasDate { get; set; }
+    }
+}
---
+++ Net21/WebPortal/DbStuff/Models/Girl.cs
@@ -0,0 +1,10 @@
+﻿namespace WebPortal.DbStuff.Models
+{
+    public class Girl : BaseModel
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public int Age { get; set; }
+        public int? Size { get; set; }
+    }
+}
