a55f25f5fdfe30fd8875fc2887ad6d49eb918356
Isyuzev <Isyuzev@yandex.by>
2025-08-11 15:57:22 +0300

    Updated

--- Net21/WebPortal/Controllers/SpaceStationController.cs
+++ Net21/WebPortal/Controllers/SpaceStationController.cs
@@ -1,12 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebPortal.Models;
+using System.Collections.Generic;
 
 namespace WebPortal.Controllers
 {
     public class SpaceStationController : Controller
     {
+        private static List<SpaceNewsModel> NewsItems = new List<SpaceNewsModel>();
+
         public IActionResult Index()
         {
+            return View(NewsItems);
+        }
+
+        [HttpGet]
+        public IActionResult News()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult News(string imageUrl, string title, string content)
+        {
+            if (!string.IsNullOrWhiteSpace(imageUrl))
+            {
+                NewsItems.Add(new SpaceNewsModel
+                {
+                    ImageUrl = imageUrl,
+                    Title = title,
+                    Content = content
+                });
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
