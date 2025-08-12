691a05bb946ecffd42f7082eb3cbe15f76e61350
AlesBoyarevich <alesboyarevich@gmail.com>
2025-08-12 11:05:11 +0300

    Added model for guitars with creating and deleting guitars

---
+++ Net21/WebPortal/DbStuff/Models/GuitarEntity.cs
@@ -0,0 +1,23 @@
+﻿using WebPortal.Models.UnderTheBridge;
+
+namespace WebPortal.DbStuff.Models
+{
+    public class GuitarEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+        public decimal Price { get; set; } = decimal.Zero;
+        public decimal Mark { get; set; } = decimal.Zero;
+        public int ReviewAmount { get; set; } = 0;
+
+        public AccessStatus Status { get; set; } = AccessStatus.No;
+
+        public enum AccessStatus
+        {
+            InShop,
+            InStock,
+            No
+        }
+    }
+}
--- Net21/WebPortal/DbStuff/WebPortalContext.cs
+++ Net21/WebPortal/DbStuff/WebPortalContext.cs
@@ -10,5 +10,24 @@ namespace WebPortal.DbStuff
 
         public DbSet<Girl> Girls { get; set; }
         public DbSet<Anime> Animes { get; set; }
+        public DbSet<GuitarEntity> Guitars { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<GuitarEntity>().HasData(
+                new GuitarEntity { Id = 1, ImageUrl = "ibanez-grg121-card.webp", Mark = 4, Name = "Ibanez GRG121", Price = 400, ReviewAmount = 101, Status = GuitarEntity.AccessStatus.InShop },
+                new GuitarEntity { Id = 2, ImageUrl = "les-paul.webp", Mark = 5, Name = "Gibson", Price = 10000, ReviewAmount = 1, Status = GuitarEntity.AccessStatus.InStock },
+                new GuitarEntity { Id = 3, ImageUrl = "cort-x100-opbk-card.webp", Mark = 2, Name = "Cort X100 OBPK", Price = 200, ReviewAmount = 22, Status = GuitarEntity.AccessStatus.No },
+                new GuitarEntity { Id = 4, ImageUrl = "ibanez-grg121-card.webp", Mark = 4, Name = "Ibanez GRG121", Price = 400, ReviewAmount = 101, Status = GuitarEntity.AccessStatus.InShop },
+                new GuitarEntity { Id = 5, ImageUrl = "les-paul.webp", Mark = 5, Name = "Gibson", Price = 10000, ReviewAmount = 1, Status = GuitarEntity.AccessStatus.InStock },
+                new GuitarEntity { Id = 6, ImageUrl = "cort-x100-opbk-card.webp", Mark = 2, Name = "Cort X100 OBPK", Price = 200, ReviewAmount = 22, Status = GuitarEntity.AccessStatus.No },
+                new GuitarEntity { Id = 7, ImageUrl = "ibanez-grg121-card.webp", Mark = 4, Name = "Ibanez GRG121", Price = 400, ReviewAmount = 101, Status = GuitarEntity.AccessStatus.InShop },
+                new GuitarEntity { Id = 8, ImageUrl = "les-paul.webp", Mark = 5, Name = "Gibson", Price = 10000, ReviewAmount = 1, Status = GuitarEntity.AccessStatus.InStock },
+                new GuitarEntity { Id = 9, ImageUrl = "cort-x100-opbk-card.webp", Mark = 2, Name = "Cort X100 OBPK", Price = 200, ReviewAmount = 22, Status = GuitarEntity.AccessStatus.No },
+                new GuitarEntity { Id = 10, ImageUrl = "ibanez-grg121-card.webp", Mark = 4, Name = "Ibanez GRG121", Price = 400, ReviewAmount = 101, Status = GuitarEntity.AccessStatus.InShop },
+                new GuitarEntity { Id = 11, ImageUrl = "les-paul.webp", Mark = 5, Name = "Gibson", Price = 10000, ReviewAmount = 1, Status = GuitarEntity.AccessStatus.InStock },
+                new GuitarEntity { Id = 12, ImageUrl = "cort-x100-opbk-card.webp", Mark = 2, Name = "Cort X100 OBPK", Price = 200, ReviewAmount = 22, Status = GuitarEntity.AccessStatus.No }
+                );
+        }
     }
 }
