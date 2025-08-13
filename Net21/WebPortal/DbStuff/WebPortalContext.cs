14d97dbb1d8bb996ddc8e3b4c20101ccbcbe0b7c
DmitVar <dmitvar@gmail.com>
2025-08-13 13:13:29 +0300

    Add connect database

--- Net21/WebPortal/DbStuff/WebPortalContext.cs
+++ Net21/WebPortal/DbStuff/WebPortalContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using WebPortal.DbStuff.Models;
+using WebPortal.DbStuff.Models.Motorcycles;
 
 namespace WebPortal.DbStuff
 {
@@ -10,5 +11,8 @@ namespace WebPortal.DbStuff
 
         public DbSet<Girl> Girls { get; set; }
         public DbSet<Anime> Animes { get; set; }
+        public DbSet<Brand> MotorcyleBrands { get; set; }
+        public DbSet<MotorcycleType> MotorcycleTypes { get; set; }
+        public DbSet<Motorcycle> Motorcycles { get; set; }
     }
 }
