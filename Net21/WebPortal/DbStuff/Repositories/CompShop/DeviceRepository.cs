7eb239a11914acadee27a86532b663bc0231af85
Ростислав Мошко <koshakcrosts@gmail.com>
2025-08-16 15:12:53 +0300

    Adding repositories and fixing errors for CompShop

---
+++ Net21/WebPortal/DbStuff/Repositories/CompShop/DeviceRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebPortal.DbStuff.Models.CompShop.Devices;
+
+namespace WebPortal.DbStuff.Repositories.CompShop
+{
+    public class DeviceRepository : BaseRepository<BaseDevice>
+    {
+        public DeviceRepository(WebPortalContext portalContext) : base(portalContext)
+        {
+        }
+
+        public List<BaseDevice> GetAllPopular()
+        {
+            return _dbSet.Where(d => d.IsPopular == true).ToList();
+        }
+
+        /// <summary>
+        /// Not Return List !!!!!
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<BaseDevice> GetDeviceWithCategoryAndTypeDevice()
+        {
+            return _dbSet
+                .Include(d => d.TypeDevice)
+                .Include(d => d.Category);
+        }
+    }
+}
