cd5e83bc7c0a3c788cc01899c60b6e984d4be2fb
knazevairina <irina_20030411@mail.ru>
2025-08-17 14:26:21 +0300

    Controller

--- Net21/WebPortal/Controllers/CdekCallRequestApiController.cs
+++ Net21/WebPortal/Controllers/CdekCallRequestApiController.cs
@@ -1,20 +1,50 @@
 using Microsoft.AspNetCore.Mvc; // Для Controller, IActionResult, HttpGet/HttpPost
 using WebPortal.DbStuff.Repositories; // Для CdekCallRequestRepository
-using WebPortal.DbStuff.Models; // Для CdekCallRequest
+using WebPortal.DbStuff.Models;
+using EnvironmentName = Microsoft.AspNetCore.Hosting.EnvironmentName; // Для CdekCallRequest
 namespace WebPortal.Controllers;
 
 public class CdekCallRequestController : Controller
 {
-    private readonly CdekCallRequestRepository _repository;
-
-    public CdekCallRequestController(CdekCallRequestRepository repository)
+    private List<int> numbers = new List<int>() {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    
+    private List<CallRequest> _requests = new List<CallRequest>() {Name, Question};
+    
+    [HttpGet]
+    public IActionResult Create()
     {
-       _repository = repository; 
+        return View();
     }
-   
+    
     [HttpPost]
     public IActionResult Create(CallRequest request)
     {
-        
+        if (ModelState.IsValid)
+        {
+            _requests.Add(request);
+            return RedirectToAction("Index");
+        }
+        return View(request);
     }
-}
+    
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View(_requests);
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    private readonly CdekCallRequestRepository _cdekRepository;
+
+    public CdekCallRequestController(CdekCallRequestRepository cdekRepository)
+    {
+        _cdekRepository = cdekRepository;
+    }
