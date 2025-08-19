1ba35a374f2c429e10fc6596cdf371873d85cf51
Daniel Sinke <danik3559@gmail.com>
2025-08-19 23:04:00 +0300

    fix controller

--- Net21/WebPortal/Program.cs
+++ Net21/WebPortal/Program.cs
@@ -22,7 +22,7 @@ builder.Services.AddDbContext<NotesDbContext>(
 // Register Repositories
 builder.Services.AddScoped<IUserRepositrory, UserRepositrory>();
 builder.Services.AddScoped<IGirlRepository, GirlRepository>();
-
+builder.Services.AddScoped<ITourismRepository, TourismRepository>();
 // Register Servcies
 // builder.Services.AddScoped<SuperService>();
 
