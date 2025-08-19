73ab271eb8a8c090ac4afcab9efac452c4ae6373
Alex-Orel34 <alex.orlovich@gmail.com>
2025-08-20 00:51:27 +0300

    Need merge

---
+++ Net21/WebPortal/Models/Marketplace/HeadphonesViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WebPortal.Models.Marketplace
+{
+    public class HeadphonesViewModel : ProductBase
+    {
+        public string ConnectionType { get; set; }
+        public bool HasNoiseCancellation { get; set; }
+        public string Impedance { get; set; }
+    }
+}
+
