b52423e2d79bf80ef3bbaecdbaf305a581445817
AlesBoyarevich <alesboyarevich@gmail.com>
2025-08-19 21:03:53 +0300

    Added new model

---
+++ Net21/WebPortal/DbStuff/Models/CommentEntity.cs
@@ -0,0 +1,16 @@
+﻿using WebPortal.DbStuff.Models;
+
+namespace UnderTheBridge.Data.Models
+{
+    public class CommentEntity: BaseModel
+    {
+        public int GuitarId { get; set; }
+        public GuitarEntity? Guitar { get; set; }
+
+        public string Message { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public decimal Mark { get; set; } = decimal.Zero;
+        public DateTime CreatedAt { get; set; }
+
+    }
+}
