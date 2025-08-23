23e40bfb93c72ad09c33141dc220b6d38198e76b
AlesBoyarevich <alesboyarevich@gmail.com>
2025-08-23 16:22:22 +0300

    Fixed bugs

--- Net21/WebPortal/Models/CustomValidationAttributtes/AntiBotAttribute.cs
+++ Net21/WebPortal/Models/CustomValidationAttributtes/AntiBotAttribute.cs
@@ -7,26 +7,24 @@ namespace WebPortal.Models.CustomValidationAttributtes
     {
         protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
         {
-            if (value is null)
+            if (value is not null)
             {
-                throw new Exception("Message can't be null");
+                if (value is not string)
+                {
+                    throw new Exception("Message can't be not string type of");
+                }
+
+                var commentsRepository = validationContext.GetRequiredService<ICommentRepository>();
+
+                var message = value as string;
+
+                var allMessages = commentsRepository.GetAll().Select(c => c.Message).ToList();
+
+                if (allMessages.Contains(message!))
+                {
+                    return new ValidationResult("You can't send comments with same message");
+                }
             }
-            if (value is not string)
-            {
-                throw new Exception("Message can't be not string type of");
-            }
-
-            var commentsRepository = validationContext.GetRequiredService<ICommentRepository>();
-
-            var message = value as string;
-
-            var allMessages = commentsRepository.GetAll().Select(c => c.Message).ToList();
-
-            if (allMessages.Contains(message!))
-            {
-                return new ValidationResult("You can't send comments with same message");
-            }
-
             return ValidationResult.Success;
         }
     }
