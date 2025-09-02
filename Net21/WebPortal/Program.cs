1edb8d7e82f8a6405ec713364e812edf47845750
Ростислав Мошко <koshakcrosts@gmail.com>
2025-09-02 18:58:11 +0300

    Add localization

--- Net21/WebPortal/Program.cs
+++ Net21/WebPortal/Program.cs
@@ -22,19 +22,19 @@ builder.Services.AddHttpLogging(opt => opt.LoggingFields = Microsoft.AspNetCore.
 builder.Logging.AddFilter("Microsoft.AspNetCore.HttpLogging", LogLevel.Information);
 
 builder.Services
-    .AddAuthentication(AuthController.AUTH_KEY)
-    .AddCookie(AuthController.AUTH_KEY, o =>
-    {
-        o.LoginPath = "/Auth/Login";
-        o.ForwardForbid = "/Auth/Forbid ";
-    });
-
-builder.Services
     .AddAuthentication(AuthNotesController.AUTH_KEY)
     .AddCookie(AuthNotesController.AUTH_KEY, o =>
     {
         o.LoginPath = "/AuthNotes/Login";
         o.ForwardForbid = "/AuthNotes/Forbid";
+    });
+
+builder.Services
+    .AddAuthentication(AuthController.AUTH_KEY)
+    .AddCookie(AuthController.AUTH_KEY, o =>
+    {
+        o.LoginPath = "/Auth/Login";
+        o.ForwardForbid = "/Auth/Forbid ";
     });
 
 // Register db context
