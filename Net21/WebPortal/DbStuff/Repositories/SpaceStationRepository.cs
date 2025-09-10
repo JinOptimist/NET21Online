351e4dff70c239d61d75e2db0199bbeb2f6a318b
Isyuzev <Isyuzev@yandex.by>
2025-09-10 11:35:57 +0300

    Updated

--- Net21/WebPortal/DbStuff/Repositories/SpaceStationRepository.cs
+++ Net21/WebPortal/DbStuff/Repositories/SpaceStationRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using WebPortal.DbStuff.Models;
 using WebPortal.DbStuff.Repositories.Interfaces;
+using WebPortal.Models.SpaceStation;
 
 namespace WebPortal.DbStuff.Repositories
 {
@@ -18,6 +19,36 @@ namespace WebPortal.DbStuff.Repositories
         public bool IsUniqTitle(string? title)
         {
             return !_dbSet.Any(x => x.Title == title);
+        }
+        public List<AuthorStatisticsViewModel> GetAuthorStatistics()
+        {
+            var sql = @"
+        WITH LastPublications AS (
+            SELECT 
+                AuthorId,
+                Title,
+                DateAdded,
+                ROW_NUMBER() OVER (PARTITION BY AuthorId ORDER BY DateAdded DESC) as rn
+            FROM SpaceNews
+            WHERE AuthorId IS NOT NULL
+        )
+        
+        SELECT 
+            u.Id as AuthorId,
+            u.UserName as AuthorName,
+            COUNT(sn.Id) as PublicationCount,
+            MAX(sn.DateAdded) as LastPublicationDate,
+            lp.Title as LastPublicationTitle
+        FROM Users u
+        INNER JOIN SpaceNews sn ON u.Id = sn.AuthorId
+        LEFT JOIN LastPublications lp ON u.Id = lp.AuthorId AND lp.rn = 1
+        GROUP BY u.Id, u.UserName, lp.Title
+        ORDER BY PublicationCount DESC";
+
+            return _portalContext.Database
+                .SqlQueryRaw<AuthorStatisticsViewModel>(sql)
+                .AsNoTracking()
+                .ToList();
         }
     }
 
