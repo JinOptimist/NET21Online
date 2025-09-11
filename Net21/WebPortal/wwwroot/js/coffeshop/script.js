8a2249227e30c8f188624299b4e6604cfabc44bd
Artiom Zelianok <artemzelenok60@gmail.com>
2025-09-11 18:01:41 +0300

    Added a script and hiding an element on spacebar press

--- Net21/WebPortal/wwwroot/js/coffeshop/script.js
+++ Net21/WebPortal/wwwroot/js/coffeshop/script.js
@@ -1,25 +1,66 @@
-let search = document.querySelector('.search-box');
+//Vanila js
+// let search = document.querySelector('.search-box');
 
-document.querySelector('#search-icon').onclick = () =>{
-    search.classList.toggle('active');
-    navbar.classList.remove('active');
-}
+// document.querySelector('#search-icon').onclick = () =>{
+//     search.classList.toggle('active');
+//     navbar.classList.remove('active');
+// }
 
-let navbar = document.querySelector('.navbar');
+// let navbar = document.querySelector('.navbar');
+// document.querySelector('#menu-icons').onclick = () =>{
+//     navbar.classList.toggle('active');
+//     search.classList.remove('active');
+// }
 
-document.querySelector('#menu-icons').onclick = () =>{
-    navbar.classList.toggle('active');
-    search.classList.remove('active');
-}
+// window.onscroll = ()=>{
+//     search.classList.remove('active');
+//     navbar.classList.remove('active');
+// }
 
-window.onscroll = ()=>{
-    search.classList.remove('active');
-    navbar.classList.remove('active');
-}
+// let headers = document.querySelector('header');
+// window.addEventListener('scroll',()=>{
+//     headers.classList.toggle('shadow',window.scrollY > 0);
+// })
 
+//JQuery
+$(document).ready(function () {
+  let $search = $(".search-box");
+  let $navbar = $(".navbar");
+  let $header = $("header");
+  let $copyright = $(".copyright");
 
-let headers = document.querySelector('header');
+  $("#search-icon").on("click", function () {
+    $search.toggleClass("active");
+    $navbar.removeClass("active");
+  });
 
-window.addEventListener('scroll',()=>{
-    headers.classList.toggle('shadow',window.scrollY > 0);
-})
+  $("#menu-icons").on("click", function () {
+    $navbar.toggleClass("active");
+    $search.removeClass("active");
+  });
+
+  $(window).on("scroll", function () {
+    $search.removeClass("active");
+    $navbar.removeClass("active");
+    $header.toggleClass("shadow", $(window).scrollTop() > 0);
+  });
+
+  // $(document).on("keydown", function (e) {
+  //   if (e.code === "Space") {
+  //     e.preventDefault(); 
+  //     $search.removeClass("active");
+  //     $navbar.removeClass("active");
+  //   }
+  // });
+
+  $copyright.on("click", function () {
+    $(this).toggleClass("active");
+  });
+
+  $(document).on("keydown", function (e) {
+    if (e.code === "Space") {
+      e.preventDefault();
+      $copyright.hide();
+    }
+  });
+});
