04185c3b704951377c00be5faad2e3089e38e246
knazevairina <irina_20030411@mail.ru>
2025-09-19 11:55:09 +0300

    program

--- Net21/WebPortal/Program.cs
+++ Net21/WebPortal/Program.cs
@@ -146,8 +146,7 @@ using (var scope = app.Services.CreateScope())
 }
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment() 
-    && Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT").ToLower() != "cdek")
+if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
