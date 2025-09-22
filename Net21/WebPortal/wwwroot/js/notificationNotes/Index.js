a47a029f2258ca00eb7088e07d39919138257fc9
alexey levadniy <levadniy@inbox.ru>
2025-09-22 16:06:10 +0300

    Fix notificationNotes/Index.js

--- Net21/WebPortal/wwwroot/js/notificationNotes/Index.js
+++ Net21/WebPortal/wwwroot/js/notificationNotes/Index.js
@@ -2,17 +2,7 @@ $(document).ready(function () {
     $('.send').click(function () {
         const message = $('[name=Message]').val();
         const url = `${baseUrl}/api/NotificationNotes/SendMessageToAll`;
-
-        $.ajax({
-            url: url,
-            type: "POST",
-            data: { message: message }, // отправляем как form-data
-            success: function (response) {
-                console.log("Уведомление отправлено:", response);
-            },
-            error: function (err) {
-                console.error("Ошибка:", err);
-            }
-        });
+        const data = { message };
+        $.post(url, data);
     });
 });
