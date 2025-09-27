99ec45960d666b2ea5e439fb80fcd25892452277
alexey levadniy <levadniy@inbox.ru>
2025-09-28 02:30:44 +0300

    Add API for Notes

---
+++ Net21/NotesApi/DbStuff/Models/Banner.cs
@@ -0,0 +1,10 @@
+namespace NotesApi.DbStuff.Models;
+
+public class Banner : BaseModel
+{
+    public string Name { get; set; }
+    public string Url { get; set; }
+    public string ImageUrl { get; set; }
+    public DateTime CreateDate { get; set; }
+    public DateTime UpdateDate { get; set; }
+}
