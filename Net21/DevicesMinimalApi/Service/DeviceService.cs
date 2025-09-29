3d37e5c4ac4680221bd03964485ebbf4d0429f28
Ростислав Мошко <koshakcrosts@gmail.com>
2025-09-29 15:24:34 +0300

    Minimal API service for devices (CompShop)

---
+++ Net21/DevicesMinimalApi/Service/DeviceService.cs
@@ -0,0 +1,31 @@
+﻿using DevicesMinimalApi.DbStuff;
+using DevicesMinimalApi.DbStuff.Model;
+using DevicesMinimalApi.DbStuff.Repository;
+using DevicesMinimalApi.ViewModels;
+
+namespace DevicesMinimalApi.Service
+{
+    public class DeviceService
+    {
+        private readonly DeviceRepository _deviceRepository;
+
+        public DeviceService(DeviceRepository deviceRepository)
+        {
+            _deviceRepository = deviceRepository;
+        }
+
+        public int CreateDevice(DeviceViewModel device)
+        {
+            var deviceDB = new Device
+            {
+                Name = device.Name,
+                CategoryName = device.CategoryName,
+                UrlImage = device.UrlImage,
+                Price = device.Price,
+            };
+
+            _deviceRepository.Add(deviceDB);
+            return deviceDB.Id;
+        }
+    }
+}
--- Net21/WebPortal/wwwroot/js/common_constants.js
+++ Net21/WebPortal/wwwroot/js/common_constants.js
@@ -1,2 +1,3 @@
 const baseUrl = 'https://localhost:7210';
 const idolBaseUrl = 'https://localhost:7193';
+const deviceBaseUrl = 'https://localhost:7001';
