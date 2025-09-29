cb1a325dd5995c657788d9df8effbddae21254b2
Ростислав Мошко <koshakcrosts@gmail.com>
2025-09-29 19:55:14 +0300

    Removing duplicate idolBaseUrls

--- Net21/WebPortal/wwwroot/js/common_constants.js
+++ Net21/WebPortal/wwwroot/js/common_constants.js
@@ -1,5 +1,4 @@
 const baseUrl = 'https://localhost:7210';
 const idolBaseUrl = 'https://localhost:7193';
-const idolBaseUrl = 'https://localhost:7193';
 const deviceBaseUrl = 'https://localhost:7001';
 const coffeProductBaseUrl = 'https://localhost:7187';
