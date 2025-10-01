21ec3822cb6fd3728ed0ef5d48f7f89e0ae10889
Pavel Lvou <pavel.lvou@hiqo-solutions.com>
2025-10-01 20:28:39 +0300

    Add simple analog of Swagger

--- Net21/WebPortal/Controllers/HomeController.cs
+++ Net21/WebPortal/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
 using WebPortal.Models;
 using WebPortal.Models.Home;
 using WebPortal.Services;
@@ -54,6 +56,45 @@ namespace WebPortal.Controllers
         public IActionResult CallRequest()
         {
             return View(); // Ищет Views/Home/CallRequest.cshtml
+        }
+
+        public IActionResult CheckGuestEndpoints()
+        {
+            var contollers = Assembly
+                .GetAssembly(typeof(HomeController))!
+                .GetTypes()
+                .Where(type => type.BaseType == typeof(Controller));
+
+            var viewModelsMain = new List<EndPointViewModel>();
+            foreach (var contoller in contollers)
+            {
+                var isAutorizeOnContollerLevel = contoller
+                    .GetCustomAttribute<AuthorizeAttribute>() != null;
+
+                var actions = contoller
+                    .GetMethods(BindingFlags.Instance | BindingFlags.Public)
+                    .Where(method => method.ReturnType == typeof(IActionResult))
+                    .Where(method =>
+                        (!isAutorizeOnContollerLevel
+                            && method.GetCustomAttribute<AuthorizeAttribute>() == null
+                        || isAutorizeOnContollerLevel
+                            && method.GetCustomAttribute<AllowAnonymousAttribute>() != null)
+
+                        && method.GetCustomAttribute<HttpPostAttribute>() != null);
+
+                var viewModels = actions
+                    .Select(method =>
+                        new EndPointViewModel
+                        {
+                            ActionName = method.Name,
+                            ContollerName = contoller.Name,
+                            ViewModelTypeName = method.GetParameters().FirstOrDefault()?.ParameterType.Name ?? "NO PARAMETERS",
+                        })
+                    .ToList();
+                viewModelsMain.AddRange(viewModels);
+            }
+
+            return View(viewModelsMain);
         }
     }
 }
---
+++ Net21/WebPortal/Models/Home/EndPointViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebPortal.Models.Home
+{
+    public class EndPointViewModel
+    {
+        public string ContollerName { get; set; }
+        public string ActionName { get; set; }
+        public string ViewModelTypeName { get; set; }
+    }
+}
