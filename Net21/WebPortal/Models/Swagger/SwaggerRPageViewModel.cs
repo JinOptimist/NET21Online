258f6ca2927225f529df45565d6a81ebdf9b63fe
Ростислав Мошко <koshakcrosts@gmail.com>
2025-10-02 21:17:26 +0300

    Adding swaggerR for webPortal

---
+++ Net21/WebPortal/Models/Swagger/SwaggerRPageViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebPortal.Models.Swagger
+{
+    public class SwaggerRPageViewModel
+    {
+        public List<SwaggerRViewModel> SwaggerRViewModels { get; set; } = new List<SwaggerRViewModel>();
+
+        public string NameProject { get; set; }
+    }
+}
---
+++ Net21/WebPortal/wwwroot/js/swagger/swaggerR.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    $('.method-header').on('click', function () {
+        var $header = $(this);
+        var $details = $header.next('.method-details');
+
+        // Переключаем текущий
+        if ($header.hasClass('active')) {
+            $header.removeClass('active');
+            $details.slideUp();
+        } else {
+            $header.addClass('active');
+            $details.slideDown();
+        }
+    });
+});
