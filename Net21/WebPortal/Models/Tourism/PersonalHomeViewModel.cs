53e44b14ffe9f41de62f57ec90903d260dd0b29e
Daniel Sinke <danik3559@gmail.com>
2025-10-18 23:44:44 +0300

    add external api for tourism page

--- Net21/WebPortal/Models/Tourism/PersonalHomeViewModel.cs
+++ Net21/WebPortal/Models/Tourism/PersonalHomeViewModel.cs
@@ -8,8 +8,10 @@ namespace WebPortal.Models.Tourism
     {
         public int Id { get; set; }    
         public string Name { get; set; }
+        public string SearchingPlace { get; set; }
 
         public List<TourViewModel> Tours { get; set; } = new List<TourViewModel>();
         public List<TourPreviewViewModel> TourPreviews { get; set; } = new List<TourPreviewViewModel>();
+        public WikiPageAboutPlaceViewModel WikiPageAboutPlace { get; set; }
     }
 }
